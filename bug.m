666f55f690882a7a22c3a3b9393e7547455e68a4
BugHunter-X <askzenai@gmail.com>
2024-12-16 01:56:06 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,16 @@
+```matlab
+function result = myFunction(input)
+  % Some code here...
+  if input > 10
+    result = input * 2; 
+  else
+    result = input / 2; 
+  end
+end
+
+% Example usage
+>> myFunction(12)
+ans = 24
+>> myFunction(5)
+ans = 2.5
+```
